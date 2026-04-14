1a0edb90316b3b4b79934b6b145c7f70a9ca8817
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-04-14 17:39:54 -0300

    Create main.py

---
+++ problems/3.Longest_substring/main.py
@@ -0,0 +1,21 @@
+# Dada uma string s, encontre o comprimento do caractere mais longo. substringSem caracteres duplicados.
+
+# Exemplo 1:
+
+# Entrada: s = "abcabcbb"
+#  Saída: 3
+#  Explicação: A resposta é "abc", com comprimento 3. Observe que "bca"e "cab"também são respostas corretas.
+# Exemplo 2:
+
+# Entrada: s = "bbbbb"
+#  Saída: 1
+#  Explicação: A resposta é "b", com comprimento 1.
+# Exemplo 3:
+
+# Entrada: s = "pwwkew"
+#  Saída: 3
+#  Explicação: A resposta é "wke", com comprimento 3. 
+# Observe que a resposta deve ser uma substring, "pwke" é uma subsequência e não uma substring.
+
+
+ 
